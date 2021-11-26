90d2c86cdfa7312c0f6e6a1635800e387e4f943d
Hung Nguyen <hdnguyen527@gmail.com>
2021-11-26 12:04:54 -0800

    created event model and seeded

--- fam_jam/config/routes.rb
+++ fam_jam/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :events
+    end
+  end
+
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- fam_jam/db/seeds.rb
+++ fam_jam/db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+titles = ["pick up the kids", "anniversary dinner", "get the dry cleaning", "Joe swimming lesson", "grandma is babysitting"]
+cities = ["Vancouver", "Richmond", "Burnaby", "Surrey", "New Westminster"]
+guests = ["joe@123.com", "jill@123.com", "lexi@123.com", "hung@123.com"]
+
+Event.destroy_all
+
+10.times do
+    Event.create(
+        title: titles.sample,
+        start_date: DateTime.now - (rand * 21),
+        end_date: DateTime.now + (rand *21),
+        location: "Some Location",
+        description: Faker::ChuckNorris.fact,
+        city: cities.sample,
+        province: "BC",
+        country: "Canada",
+        latitude: 49.2,
+        longitude: 100.3,
+        guest_list: guests
+    )
+end
+
+
+
+events = Event.all
+
+puts Cowsay.say("Generated #{events.count} events", :frogs)
+
+
